c694a7c326ca4c64a9411885af39c2ff7c95b050
mirai <Mirai301@users.noreply.github.com>
2022-05-15 10:44:15 +0900

    スクリプトの修正

--- port_scan.py
+++ port_scan.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python
 import sys
+from unittest import skip
 import nmap
 
-print('[*]Usage: $ port_scan.py 192.168.xx.xx 10000')
+try:
+    args = sys.argv
+    ip = str(args[1])
+    port = args[2]
+    ports = list()
+except:
+    print('[*]Usage: $ port_scan.py 192.168.xx.xx 10000', end="\n\n")
 
-args = sys.argv
-ip = str(args[1])
-port = args[2]
-ports = list()
+def port_split(port):
+    for i in range(0, int(port), 2000):
+        ports.append(i)
 
-for i in range(0, int(port), 2000):
-    ports.append(i)
+    last_port = int(port) - i + int(ports[-1])
+    ports.append(last_port)
+    return ports
 
-last_port = int(port) - i + int(ports[-1])
-ports.append(last_port)
+def main(port):
+    ports = port_split(port)
+    print('scan_port: 1-' + str(port))
+    for i in range(1, len(ports)):
+        port_num = str(str(ports[i-1]) + '-' + str(ports[i]))
+        
+        nm = nmap.PortScanner()
+        nm.scan(ip, port_num, '-sV -sC -A')
 
-for i in range(1, len(ports)):
-    port_ = str(str(ports[i-1]) + '-' + str(ports[i]))
-    print('scan_port: ' + port_)
-    nm = nmap.PortScanner()
-    nm.scan(ip, port_, '-sV -sC -A')
-    print(nm.command_line())
+        if i == 1:
+            # print(nm.command_line())
+            print('----------------------------------------------------')
+        else:
+            next
 
-    for host in nm.all_hosts():
-        print('----------------------------------------------------')
-        print('Host : %s (%s)' % (host, nm[host].hostname()))
-        print('State : %s' % nm[host].state())
-        for proto in nm[host].all_protocols():
-            print('----------')
-            print('Protocol : %s' % proto)
+        for host in nm.all_hosts():
+            for proto in nm[host].all_protocols():
+                lport = nm[host][proto].keys()
+                sorted(lport)
+                for port in lport:
+                    print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
 
-            lport = nm[host][proto].keys()
-            sorted(lport)
-            for port in lport:
-                print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
-        print('----------------------------------------------------', end="\n")
+main(port)
